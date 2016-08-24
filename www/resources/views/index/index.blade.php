576434ab719198c433c7b5ea5c426dbde6095bc0
Siebren Kranenburg <info@siebrenkranenburg.nl>
2016-08-24 16:51:23 +0200

    added coloured pointers for dangerized values && forecast layout

--- www/resources/views/index/index.blade.php
+++ www/resources/views/index/index.blade.php
@@ -4,24 +4,41 @@
 	<div id="map"></div>
 
 	<div id="sidebar">
-		<h3 class='js-station-name'>&nbsp;</h3>
-		<div class="panel panel-default">
-			<div class="panel-heading">
-				<h3 class="panel-title">Current weather</h3>
+		<h3 class='js-station-name'>--</h3>
+		<div class="panel-group" id="accordion" role="tablist" area-multiselectable="true">
+			<div class="panel panel-default">
+				<div class="panel-heading" role='button' data-toggle="collapse" data-parent="#accordion" href="#currentWeather" aria-expanded="true" aria-controls="currentWeather">
+					<h4>Current weather <i class="glyphicon glyphicon-chevron-down pull-right"></i></h4>
+				</div>
+				<div id='currentWeather' class='panel-collapse collapse in' role='tabpanel' aria-labelledby='currentWeather'>
+					<div class="panel-body">
+						<h3>Wind: <span class='js-station-current-wind'></span> km/h</h3>
+						<h3>Gusts: <span class='js-station-current-gust'></span> km/h</h3>
+						<p class='js-station-current-weather'>&nbsp;</p>
+					</div>
+				</div>
 			</div>
-			<div class="panel-body">
-				<h1 class='js-station-current-temp'>&nbsp;</h1>
-				<p class='js-station-current-weather'>&nbsp;</p>
+			<div class="panel panel-default">
+				<div class='panel-heading collapsed' role="button" id='headingTwo' data-toggle="collapse" data-parent="#accordion" href="#forecast" aria-expanded="true" aria-controls="forecast">
+					<h4>Forecast <i class="glyphicon glyphicon-chevron-down pull-right"></i></h4>
+				</div>
+				<div id='forecast' class='panel-collapse collapse' role='tabpanel' aria-labelledby='forecast'>
+					<div class="panel-body">
+						<h3>Wind: <span class='js-station-forecast-wind'></span> km/h</h3>
+						<h3>Gusts: <span class='js-station-forecast-gust'></span> km/h</h3>
+						<p class='js-station-forecast-weather'>&nbsp;</p>
+					</div>
+				</div>
 			</div>
-		</div>
-		<div class="panel panel-default">
-			<div class="panel-heading">
-				<h3 class="panel-title">Forecast</h3>
-			</div>
-		</div>
-		<div class="panel panel-default">
-			<div class="panel-heading">
-				<h3 class="panel-title">Graph</h3>
+			<div class="panel panel-default">
+				<div class='panel-heading collapsed' role="button" data-toggle="collapse" data-parent="#accordion" href="#graphs" aria-expanded="true" aria-controls="graphs">
+					<h4>Graphs <i class="glyphicon glyphicon-chevron-down pull-right"></i></h4>
+				</div>
+				<div id='graphs' class='panel-collapse collapse' role='tabpanel' aria-labelledby='graphs'>
+					<div class="panel-body">
+						the graphs
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
--- www/resources/views/layout/default.blade.php
+++ www/resources/views/layout/default.blade.php
@@ -5,27 +5,27 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>IHS Dashboard</title>
+	<title>Project Dashibodi</title>
 	<link rel="stylesheet" href="/assets/main.min.css" type="text/css">
 </head>
 <body>
 
 	<nav class="navbar navbar-default navbar-fixed-top">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="/">IHS Dashboard</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li><a href="/download">Downloads</a></li>
-          </ul>
-        </div>
-    </nav>
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="/">My Dashibodi</a>
+		</div>
+		<div id="navbar" class="collapse navbar-collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="/download">Downloads</a></li>
+			</ul>
+		</div>
+	</nav>
 
 	@yield('content')
 
